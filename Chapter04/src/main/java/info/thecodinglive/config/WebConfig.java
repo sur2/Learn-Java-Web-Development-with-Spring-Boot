40dd5e2c5b65a022fa8ffe61f4471b8e16350b10
정표용 <sur2@naver.com>
2020-02-14 23:46:52 +0900

    thymeleaf success

--- Chapter04/src/main/java/info/thecodinglive/config/WebConfig.java
+++ Chapter04/src/main/java/info/thecodinglive/config/WebConfig.java
@@ -6,17 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		// '/**' : 0개 이상의 디렉터리와 매칭 ↔ '/*' : 0개 이상의 파일과 매칭
 		registry.addResourceHandler("/assets/**")
-		.addResourceLocations("classpath:/assets/", "/assets/")
-		.setCachePeriod(60*60*24*365) // 캐시 기간 1년
-		.resourceChain(true)
-		// VersionResourceResolver().addContentVersionStrategy는 웹 리소스 파일 로드 시 해쉬 적용
-		.addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
-		;
+				.addResourceLocations("classpath:/assets/", "/assets/")
+				.setCachePeriod(60 * 60 * 24 * 365) // 캐시 기간 1년
+				.resourceChain(true)
+				// VersionResourceResolver().addContentVersionStrategy는 웹 리소스 파일 로드 시 해쉬 적용
+				.addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
 	}
 }
