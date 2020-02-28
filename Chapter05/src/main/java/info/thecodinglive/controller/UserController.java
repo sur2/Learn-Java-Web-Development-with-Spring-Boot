2f0b10095edb62e975c1f5503ed3ac2163dacacc
정표용 <sur2@naver.com>
2020-02-28 23:22:46 +0900

    REST

--- Chapter05/src/main/java/info/thecodinglive/controller/UserController.java
+++ Chapter05/src/main/java/info/thecodinglive/controller/UserController.java
@@ -1,13 +1,43 @@
 package info.thecodinglive.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.thecodinglive.model.User;
 
 @RestController
 public class UserController {
-	private static List<User> userList = new ArrayList<User>();
+	private static List<User> userList = new ArrayList<User>(); 
+	// {} 인스턴스 초기화블록 : 클래스의 생성자 호출 이전에 인스턴스가 생성될 때마다 실행
+	{
+		userList.add(new User(1, "jpub01", "user01@test.com", "remine", new Date()));
+		userList.add(new User(2, "jpub02", "user02@test.com", "restart", new Date()));
+		userList.add(new User(3, "jpub03", "user03@test.com", "nine", new Date()));
+		userList.add(new User(4, "jpub04", "user04@test.com", "namu", new Date()));
+	}
+	// static{} 클래스 초기화 블록 : 클래스가 처음 로딩 될 때 한 번 실행
+	static {
+		userList.add(new User(0, "jpub00", "user00@test.com", "zero", new Date()));
+	}
+	
+	@RequestMapping(value = "/user/{userNo}")
+	public ResponseEntity<User> getUesrInfo(@PathVariable(name = "userNo") int userNo) {
+		User user = userList.get(userNo);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/user") 
+	public ResponseEntity<List<User>> getUserList() {
+		HashMap<String, Object> resultList = new HashMap<String, Object>();
+		resultList.put("result", userList);
+		return new ResponseEntity(resultList, HttpStatus.OK);
+	}
 }
---
+++ Chapter05/src/test/java/RestAPITest.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import info.thecodinglive.Chapter05Application;
+import info.thecodinglive.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Chapter05Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestAPITest {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testGetUserById() {
+		String url = "http://localhost:9090/user/0";
+		User user = restTemplate.getForObject(url, User.class);
+		System.out.println("등록일:" + user.getRegDate() + "," + user.getUserId() + "," + user.getUname());
+	}
+	
+	@Test
+	public void testGetUsers() {
+		String url = "http://localhost:9090/user/";
+		ResponseEntity<Map<String, List<User>>> result = restTemplate
+				.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, List<User>>>() {
+				});
+		Map<String, List<User>> tempMap = (Map)result.getBody();
+		
+		ArrayList<User> resultArr = (ArrayList<User>) tempMap.get("result");
+		for (User usr : resultArr) {
+			System.out.println(usr.getUname());
+		}
+	}
+}
