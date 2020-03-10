ad77b199c8712b195f7df07d355d5a76768738ae
정표용 <sur2@naver.com>
2020-03-10 23:04:47 +0900

    Mybatis INSERT

--- Chapter06MyBatis/src/main/java/info/thecodinglive/MybatisMain.java
+++ Chapter06MyBatis/src/main/java/info/thecodinglive/MybatisMain.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
+import info.thecodinglive.model.UserVO;
 import info.thecodinglive.repository.UserRepository;
 
 @SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
@@ -19,6 +20,11 @@ public class MybatisMain implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
+		UserVO userVO = new UserVO("test4", "mak", "1234");
+		userRepository.addUserInfo(userVO);
+		System.out.println("==입력 후==");
 		System.out.println(userRepository.getUserInfoAll().toString());
+		System.out.println("==like 이름 검색==");
+		System.out.println(userRepository.findByUserName("ki").toString());
 	}
 }
--- Chapter06MyBatis/src/main/java/info/thecodinglive/repository/UserRepository.java
+++ Chapter06MyBatis/src/main/java/info/thecodinglive/repository/UserRepository.java
@@ -1,19 +1,36 @@
 package info.thecodinglive.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import info.thecodinglive.model.UserVO;
+
 @Repository
 public class UserRepository {
-	private static final String MAPPER_NAME_SPACE="sample.mapper.userMapper";
+	private static final String MAPPER_NAME_SPACE="sample.mapper.userMapper.";
 	
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	
 	public List getUserInfoAll() {
-		return sqlSessionTemplate.selectList(MAPPER_NAME_SPACE + ".selectUserInfoAll");
+		return sqlSessionTemplate.selectList(MAPPER_NAME_SPACE + "selectUserInfoAll");
+	}
+	
+	public int addUserInfo(UserVO userVO) {
+		int result = sqlSessionTemplate.insert(MAPPER_NAME_SPACE + "addUserInfo", userVO);
+		System.out.println("INSERT RESULT:: "+result);
+		return result;
+	}
+	
+	public List findByUserName(String userName) {
+		// KEY, VALUE
+		Map<String, Object> params = new HashMap();
+		params.put("userName", userName);
+		return sqlSessionTemplate.selectList(MAPPER_NAME_SPACE + "findByUserNameLike", params);
 	}
 }
